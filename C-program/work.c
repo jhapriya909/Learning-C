df4b6410c78bc126c03ccef64254f66c41a24143
jhapriya909 <thepriyajha8840@gmail.com>
2025-01-21 21:59:25 +0530

    d:\program.c\Learning-C\C-program\work.c

---
+++ C-program/work.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+printf("enter input");
+
+{
+ printf("Raj");
+}
+{
+printf("Rani");
+}
+{printf("monu");
+}
+
+{printf("mona");
+}
+
+
+{printf("pawan");
+}
+return 0;
+}
