c31b64aed7cf6a53345640b23125351d93a382f6
jhapriya909 <thepriyajha8840@gmail.com>
2025-08-28 21:51:04 +0530

    d:\program.c\Learning-C\C-program\MultiplyTwo FloatingPointNumbers.c

---
+++ C-program/MultiplyTwo FloatingPointNumbers.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    float a, b, product;
+    printf("Enter two numbers: ");
+    scanf("%f %f", &a, &b);
+    product = a * b;
+    printf("Product = %.2f", product);
+    return 0;
+}
