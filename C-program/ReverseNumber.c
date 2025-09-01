86e92a850e0edb45ff2caa8d100d8a244700d8ec
jhapriya909 <thepriyajha8840@gmail.com>
2025-09-01 21:03:49 +0530

    d:\program.c\Learning-C\C-program\ReverseNumber.c

---
+++ C-program/ReverseNumber.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, rev = 0, digit;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while(n > 0) {
+        digit = n % 10;      // last digit
+        rev = rev * 10 + digit; 
+        n = n / 10;          // remove last digit
+    }
+
+    printf("Reversed number = %d\n", rev);
+    return 0;
+}
