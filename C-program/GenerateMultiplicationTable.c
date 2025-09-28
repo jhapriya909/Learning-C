060f354a012dd607765ba7ef03616209cb894a0c
jhapriya909 <thepriyajha8840@gmail.com>
2025-09-28 22:40:42 +0530

    d:\program.c\Learning-C\C-program\GenerateMultiplicationTable.c

---
+++ C-program/GenerateMultiplicationTable.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+// Function to print multiplication table
+void multiplicationTable(int n) {
+    printf("Multiplication Table of %d:\n", n);
+    for(int i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", n, i, n * i);
+    }
+}
+
+int main() {
+    multiplicationTable(7);
+    return 0;
+}
+#include <stdio.h>
+
+// Function to print multiplication table
+void multiplicationTable(int n) {
+    printf("Multiplication Table of %d:\n", n);
+    for(int i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", n, i, n * i);
+    }
+}
+
+int main() {
+    multiplicationTable(7);
+    return 0;
+}
